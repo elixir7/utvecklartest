6ddf2b97cedc5b6dd6dcf1a0ddd96e57b81b8677
Isak <aslundisak@gmail.com>
2015-11-26 14:59:08 +0100

    Added google api key and first basic initializers for checking location

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,47 @@
 $( document ).ready(function() {
 
-  // Get started!
+	var myLatLng;
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    }
+	}
+
+	//Geolocation information grabbing
+	function showPosition(position) {
+		myLatLng = {lat: position.coords.latitude, lng: position.coords.longitude}
+		initMap();
+	}
+
+	function initMap() {
+		element = document.getElementById('map');
+		var options = {
+			center: myLatLng,
+			zoom: 15
+		};
+		var map = new google.maps.Map(element, options);
+
+	  	var marker = new google.maps.Marker({
+	    	position: myLatLng,
+	    	map: map,
+	    	draggable: true,
+    		animation: google.maps.Animation.DROP,
+	    	title: 'Hello World!'
+	  	});
+	  	marker.addListener('click', toggleBounce);
+		console.log(myLatLng);
+	}
+
+	function toggleBounce() {
+  		if (marker.getAnimation() !== null) {
+    		marker.setAnimation(null);
+  		} else {
+    		marker.setAnimation(google.maps.Animation.BOUNCE);
+  		}
+	}
+
+	$("#button").click(getLocation);
+
+
 
 });
